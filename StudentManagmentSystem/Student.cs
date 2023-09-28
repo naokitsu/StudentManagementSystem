a49e055ee0de5746cd368225850736e61f5a1b04
naokitsu <naokitsu@tutanota.com>
2023-09-28 12:38:13 +0300

    Add ToString to Student

--- StudentManagmentSystem/Student.cs
+++ StudentManagmentSystem/Student.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices.ComTypes;
+
 namespace StudentManagmentSystem;
 
 public class Student
@@ -6,11 +8,19 @@ public class Student
     public String SecondName { get; set; }
     public uint Age { get; set; }
 
-    public Student(String firstName, String secondName, uint age)
+    public uint ID { get; set; }
+    
+    public Student(String firstName, String secondName, uint age, uint id)
     {
         FirstName = firstName;
         SecondName = secondName;
         Age = age;
+        ID = id;
     }
+
+    public override string ToString()
+    {
+        return String.Format("{0} {1} (ID: {3}, Возраст: {2})", FirstName, SecondName, Age, ID);
+    }  
     
 }
